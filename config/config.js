bd517277be4bb58da0f4b15791af349c90ef68dd
jackson john <jackson.john@inapp.com>
2019-12-05 10:09:43 +0530

    project structure created

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-const config;
+var config;
 if (process.env.NODE_ENV == 'production') {
     config = require('./prod');
 } else {
--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const app = express();
+const db = require('./config/database');
 const bodyparser = require("body-parser");
-const routes = require('./routes/routes')
+const routes = require('./routes/routes');
 
+//database connection
+db.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err);
+        return;
+    }
+    console.log('DB connected Successfully');
+});
+
+//For parsing REST api body
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
-app.get('/',routes);
+
+// Main Router
+app.get('/api',routes);
 
 const port = process.env.PORT || 5000;
 
+//Server start
 app.listen(port, () => {
     console.log(`Server start running on port ${port}`);
 });
