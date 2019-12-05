4eb999b291e8f21615a379fc8a49d6234ce3ad8a
jackson john <jackson.john@inapp.com>
2019-12-05 11:20:36 +0530

    db changed to postgres

--- client/src/components/loader.js
+++ client/src/components/loader.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react';
+import DotLoader from 'react-spinners/DotLoader';
 
 const loader = (props) => {
 
+    const [loading, setloading] = useState(false);
+
+    return (
+        <div className={"layout " + loading ? 'show' : ''}>
+            <DotLoader></DotLoader>
+        </div>
+    )
 }
 
 export default loader;
--- config/database.js
+++ config/database.js
@@ -1,11 +1,12 @@
-const mysql = require('mysql');
+const { Pool } = require('pg')
 const config = require('./config');
 
-var connection = mysql.createConnection({
-  host     : config.host,
-  user     : config.user,
-  password : config.password,
-  database : config.database
-});
- 
-module.exports = connection;
+const pool = new Pool({
+    user: config.user,
+    host: config.host,
+    database: config.database,
+    password: config.password,
+    port: 5432,
+  })
+  
+module.exports = pool;
